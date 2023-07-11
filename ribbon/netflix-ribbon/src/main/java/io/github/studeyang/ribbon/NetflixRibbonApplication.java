5bb1ef2bb17c1e7888c86200bdf126305e6a58bb
杨渌渌#A02918 <lulu.yang@casstime.com>
2023-07-11 18:28:35 +0800

    netflix ribbon

---
+++ ribbon/netflix-ribbon/src/main/java/io/github/studeyang/ribbon/NetflixRibbonApplication.java
@@ -0,0 +1,13 @@
+package io.github.studeyang.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixRibbonApplication.class, args);
+    }
+
+}
---
+++ ribbon/netflix-ribbon/src/test/java/io/github/studeyang/ribbon/NetflixRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.studeyang.ribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
